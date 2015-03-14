940fc78f14dfa38eee492fc7838c64acefce4705
Emati Mitame <gitcommits@mita.me>
2015-03-14 21:55:11 +0000

    Added password hashing and storage. Added create login screen. Added icon to windows.

--- Training Logger/Training Logger/FrmCreateLogin.Designer.vb
+++ Training Logger/Training Logger/FrmCreateLogin.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class FrmLogin
+Partial Class FrmCreateLogin
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,15 +22,27 @@ Partial Class FrmLogin
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmLogin))
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmCreateLogin))
+        Me.TxtUsername = New System.Windows.Forms.TextBox()
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.TxtPassword = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.TxtUsername = New System.Windows.Forms.TextBox()
         Me.ButtonSubmit = New System.Windows.Forms.Button()
+        Me.TxtPassConfirm = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'TxtUsername
+        '
+        Me.TxtUsername.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.SetColumnSpan(Me.TxtUsername, 2)
+        Me.TxtUsername.Location = New System.Drawing.Point(100, 3)
+        Me.TxtUsername.Name = "TxtUsername"
+        Me.TxtUsername.Size = New System.Drawing.Size(240, 20)
+        Me.TxtUsername.TabIndex = 1
         '
         'TableLayoutPanel1
         '
@@ -45,15 +57,18 @@ Partial Class FrmLogin
         Me.TableLayoutPanel1.Controls.Add(Me.Label1, 0, 1)
         Me.TableLayoutPanel1.Controls.Add(Me.Label2, 0, 0)
         Me.TableLayoutPanel1.Controls.Add(Me.TxtUsername, 1, 0)
-        Me.TableLayoutPanel1.Controls.Add(Me.ButtonSubmit, 2, 2)
-        Me.TableLayoutPanel1.Location = New System.Drawing.Point(13, 13)
+        Me.TableLayoutPanel1.Controls.Add(Me.ButtonSubmit, 2, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.TxtPassConfirm, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label3, 0, 2)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(12, 12)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
-        Me.TableLayoutPanel1.RowCount = 3
+        Me.TableLayoutPanel1.RowCount = 4
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(299, 83)
-        Me.TableLayoutPanel1.TabIndex = 0
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(343, 111)
+        Me.TableLayoutPanel1.TabIndex = 1
         '
         'TxtPassword
         '
@@ -61,10 +76,10 @@ Partial Class FrmLogin
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TxtPassword.CausesValidation = False
         Me.TableLayoutPanel1.SetColumnSpan(Me.TxtPassword, 2)
-        Me.TxtPassword.Location = New System.Drawing.Point(64, 29)
+        Me.TxtPassword.Location = New System.Drawing.Point(100, 29)
         Me.TxtPassword.Name = "TxtPassword"
         Me.TxtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(8226)
-        Me.TxtPassword.Size = New System.Drawing.Size(232, 20)
+        Me.TxtPassword.Size = New System.Drawing.Size(240, 20)
         Me.TxtPassword.TabIndex = 2
         Me.TxtPassword.WordWrap = False
         '
@@ -86,49 +101,58 @@ Partial Class FrmLogin
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "Username"
         '
-        'TxtUsername
-        '
-        Me.TxtUsername.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.TableLayoutPanel1.SetColumnSpan(Me.TxtUsername, 2)
-        Me.TxtUsername.Location = New System.Drawing.Point(64, 3)
-        Me.TxtUsername.Name = "TxtUsername"
-        Me.TxtUsername.Size = New System.Drawing.Size(232, 20)
-        Me.TxtUsername.TabIndex = 1
-        '
         'ButtonSubmit
         '
         Me.ButtonSubmit.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.ButtonSubmit.Location = New System.Drawing.Point(222, 55)
+        Me.ButtonSubmit.Location = New System.Drawing.Point(264, 81)
         Me.ButtonSubmit.Name = "ButtonSubmit"
-        Me.ButtonSubmit.Size = New System.Drawing.Size(74, 25)
-        Me.ButtonSubmit.TabIndex = 3
-        Me.ButtonSubmit.Text = "Login"
+        Me.ButtonSubmit.Size = New System.Drawing.Size(76, 27)
+        Me.ButtonSubmit.TabIndex = 4
+        Me.ButtonSubmit.Text = "Create user"
         Me.ButtonSubmit.UseVisualStyleBackColor = True
         '
-        'FrmLogin
+        'TxtPassConfirm
+        '
+        Me.TxtPassConfirm.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.SetColumnSpan(Me.TxtPassConfirm, 2)
+        Me.TxtPassConfirm.Location = New System.Drawing.Point(100, 55)
+        Me.TxtPassConfirm.Name = "TxtPassConfirm"
+        Me.TxtPassConfirm.PasswordChar = Global.Microsoft.VisualBasic.ChrW(8226)
+        Me.TxtPassConfirm.Size = New System.Drawing.Size(240, 20)
+        Me.TxtPassConfirm.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 52)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Confirm Password"
+        '
+        'FrmCreateLogin
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(324, 108)
+        Me.ClientSize = New System.Drawing.Size(367, 135)
         Me.Controls.Add(Me.TableLayoutPanel1)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.MaximizeBox = False
-        Me.Name = "FrmLogin"
-        Me.Text = "Training Logger - Login"
+        Me.Name = "FrmCreateLogin"
+        Me.Text = "Training Logger - Create Login"
         Me.TableLayoutPanel1.ResumeLayout(False)
         Me.TableLayoutPanel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents TxtUsername As System.Windows.Forms.TextBox
     Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
     Friend WithEvents TxtPassword As System.Windows.Forms.TextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents TxtUsername As System.Windows.Forms.TextBox
     Friend WithEvents ButtonSubmit As System.Windows.Forms.Button
-
+    Friend WithEvents TxtPassConfirm As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
 End Class
--- Training Logger/Training Logger/FrmLogin.vb
+++ Training Logger/Training Logger/FrmLogin.vb
@@ -3,4 +3,37 @@
     Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles TableLayoutPanel1.Paint
 
     End Sub
+
+    Private Sub FrmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Not ModControl.Login.loadLogin() Then
+            MsgBox("As this is the first time you have launched this program, you will have to create a new user." & vbCrLf &
+                   "Please enter a memorable username and password.")
+            Dim newForm As New FrmCreateLogin
+            newForm.Show()
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub ButtonSubmit_Click(sender As Object, e As EventArgs) Handles ButtonSubmit.Click
+        If ModControl.Login.checkLogin(TxtUsername.Text, TxtPassword.Text) Then
+            MsgBox("Success!")
+        Else
+            MsgBox("The username and password do not match. Please try again.")
+            TxtPassword.Text = ""
+        End If
+    End Sub
+
+    Private Sub TxtPassword_KeyDown(sender As Object, e As KeyEventArgs) Handles TxtPassword.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            e.SuppressKeyPress = True
+            ButtonSubmit.PerformClick()
+        End If
+    End Sub
+
+    Private Sub TxtUsername_KeyDown(sender As Object, e As KeyEventArgs) Handles TxtUsername.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            e.SuppressKeyPress = True
+            TxtPassword.Focus()
+        End If
+    End Sub
 End Class
