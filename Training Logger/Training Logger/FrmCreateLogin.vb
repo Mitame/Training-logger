00a93e75db58351cab58b96d9ae5a76983a725a2
MrMindImplosion <leviwright1997@googlemail.com>
2015-03-14 21:55:11 +0000

    Added password hashing and storage. Added create login screen. Added icon to windows.

---
+++ Training Logger/Training Logger/FrmCreateLogin.vb
@@ -0,0 +1,44 @@
+﻿Public Class FrmCreateLogin
+
+    Private Sub TxtPassConfirm_KeyDown(sender As Object, e As KeyEventArgs) Handles TxtPassConfirm.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            e.SuppressKeyPress = True
+            ButtonSubmit.PerformClick()
+        End If
+    End Sub
+
+    Private Sub TxtPassword_KeyDown(sender As Object, e As KeyEventArgs) Handles TxtPassword.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            e.SuppressKeyPress = True
+            TxtPassConfirm.Focus()
+        End If
+    End Sub
+
+    Private Sub TxtUsername_KeyDown(sender As Object, e As KeyEventArgs) Handles TxtUsername.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            e.SuppressKeyPress = True
+            TxtPassword.Focus()
+        End If
+    End Sub
+
+    Private Sub ButtonSubmit_Click(sender As Object, e As EventArgs) Handles ButtonSubmit.Click
+        If TxtUsername.Text = "" Then
+            MsgBox("Please enter a username.")
+        End If
+
+        If TxtPassword.Text = "" Then
+            MsgBox("Please enter a password.")
+            Return
+        End If
+
+        If TxtPassword.Text <> TxtPassConfirm.Text Then
+            MsgBox("The passwords do not match. Please re-enter them and make sure they are the same.")
+            TxtPassword.Text = ""
+            TxtPassConfirm.Text = ""
+            Return
+        End If
+
+        ModControl.Login.saveLogin(TxtPassword.Text, TxtUsername.Text)
+        MsgBox("Saved!")
+    End Sub
+End Class
---
+++ Training Logger/Training Logger/ModControl.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Text
+Imports System.Security.Cryptography
+
+Module ModControl
+
+    Public Class Login
+        Shared passwordHash As String
+        Shared username As String
+        Shared loginLocation As String = My.Computer.FileSystem.CombinePath(System.Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "JLC\TrainingLog\login")
+
+        Public Shared Sub createDirectory()
+            My.Computer.FileSystem.CreateDirectory(My.Computer.FileSystem.GetParentPath(loginLocation))
+        End Sub
+
+        Public Shared Sub saveLogin(password As String, username As String)
+            My.Computer.FileSystem.WriteAllText(Login.loginLocation, username & vbCrLf & Login.GenerateHash(password), False)
+        End Sub
+
+        Public Shared Function loadLogin()
+            Try
+                Dim loginFile = My.Computer.FileSystem.ReadAllText(Login.loginLocation)
+                Dim temp = loginFile.Split(vbCrLf)
+
+                Console.WriteLine(temp.Length)
+                Login.username = temp(0)
+                Login.passwordHash = temp(1).Substring(1, temp(1).Length - 1)
+            Catch exception As System.IO.DirectoryNotFoundException
+                Login.createDirectory()
+                Return False
+            Catch exception As System.IO.FileNotFoundException
+                Return False
+            End Try
+            Return True
+
+        End Function
+        Private Shared Function GenerateHash(ByVal SourceText As String) As String
+            'Create an encoding object to ensure the encoding standard for the source text
+            Dim Ue As New UnicodeEncoding()
+            'Retrieve a byte array based on the source text
+            Dim ByteSourceText() As Byte = Ue.GetBytes(SourceText)
+            'Instantiate an MD5 Provider object
+            Dim sha As New SHA256CryptoServiceProvider()
+            'Compute the hash value from the source
+            Dim ByteHash() As Byte = sha.ComputeHash(ByteSourceText)
+            'And convert it to String format for return
+            Return Convert.ToBase64String(ByteHash)
+        End Function
+
+        Public Shared Function checkLogin(username As String, password As String)
+            If username = Login.username And GenerateHash(password) = Login.passwordHash Then
+                Return True
+            Else
+                Console.WriteLine(username & " = " & Login.username)
+                Console.WriteLine(GenerateHash(password) & " = " & Login.passwordHash)
+                Return False
+            End If
+
+        End Function
+
+
+    End Class
+
+    
+End Module
+
+
+
+
+
