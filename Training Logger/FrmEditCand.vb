e2cce9fa69586368b7bc43800e688227a4556ef6
Emati Mitame <gitcommits@mita.me>
2015-04-16 21:53:02 +0100

    FrmEditCand now shows candidates

--- Training Logger/FrmEditCand.vb
+++ Training Logger/FrmEditCand.vb
@@ -12,8 +12,28 @@
     End Sub
 
 
-    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
         Dim newform As New FrmEditUser(1)
         newform.Show()
     End Sub
+
+    Private Sub FrmEditCand_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        loadUsers()
+    End Sub
+
+    Private Sub loadUsers()
+        LbData.Items.Clear()
+        LbData.Items.Add("Username".PadRight(20) & vbTab & "First Name".PadRight(20) & vbTab & "Last Name".PadRight(20) & vbTab & "Contact Details".PadRight(40) & vbTab & "Password Hash")
+        For userID As Integer = 0 To Login.users.Length - 1
+            Dim user As Login.User = Login.getUser(userID)
+
+
+            LbData.Items.Add(user.username.PadRight(20) & vbTab &
+                             user.firstName.PadRight(20) & vbTab &
+                             user.lastName.PadRight(20) & vbTab &
+                             user.contact.PadRight(40) & vbTab &
+                             user.passwordHash)
+        Next userID
+    End Sub
+
 End Class
