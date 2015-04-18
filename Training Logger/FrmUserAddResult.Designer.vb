7cc916bbb5fc160dd8bc69a489b985188c7db852
Emati Mitame <gitcommits@mita.me>
2015-04-18 14:17:56 +0100

    Added results collection and viewing.

---
+++ Training Logger/FrmUserAddResult.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmUserAddResult
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CmBSport = New System.Windows.Forms.ComboBox()
+        Me.BtnAdd = New System.Windows.Forms.Button()
+        Me.TxtTime = New System.Windows.Forms.TextBox()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Label2, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label3, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.CmBSport, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.BtnAdd, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.TxtTime, 1, 1)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(13, 13)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 3
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(274, 82)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Activity"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 27)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Time (Seconds)"
+        '
+        'CmBSport
+        '
+        Me.CmBSport.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.CmBSport.FormattingEnabled = True
+        Me.CmBSport.Items.AddRange(New Object() {"Swimming", "Running", "Cycling"})
+        Me.CmBSport.Location = New System.Drawing.Point(90, 3)
+        Me.CmBSport.Name = "CmBSport"
+        Me.CmBSport.Size = New System.Drawing.Size(181, 21)
+        Me.CmBSport.TabIndex = 3
+        '
+        'BtnAdd
+        '
+        Me.BtnAdd.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnAdd.Location = New System.Drawing.Point(90, 56)
+        Me.BtnAdd.Name = "BtnAdd"
+        Me.BtnAdd.Size = New System.Drawing.Size(181, 23)
+        Me.BtnAdd.TabIndex = 9
+        Me.BtnAdd.Text = "Add"
+        Me.BtnAdd.UseVisualStyleBackColor = True
+        '
+        'TxtTime
+        '
+        Me.TxtTime.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TxtTime.Location = New System.Drawing.Point(90, 30)
+        Me.TxtTime.Name = "TxtTime"
+        Me.TxtTime.Size = New System.Drawing.Size(181, 20)
+        Me.TxtTime.TabIndex = 10
+        Me.TxtTime.Text = "0.0"
+        '
+        'FrmUserAddResult
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(299, 107)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "FrmUserAddResult"
+        Me.Text = "Training Log - Add Result"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents CmBSport As System.Windows.Forms.ComboBox
+    Friend WithEvents BtnAdd As System.Windows.Forms.Button
+    Friend WithEvents TxtTime As System.Windows.Forms.TextBox
+End Class
---
+++ Training Logger/ModResults.vb
@@ -0,0 +1,83 @@
+﻿Module ModResults
+    Dim allResults() As result
+    Dim hasLoaded As Boolean = False
+    Dim resultsLocation As String = My.Computer.FileSystem.CombinePath(System.Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "JLC\TrainingLog\results")
+
+    Public Class result
+        Dim resultID As Integer
+        Dim userID As Integer
+        Dim timestamp As Date
+        Dim time As Decimal
+        Dim sport As String
+
+        Public Sub New(resultID As Integer, userID As Integer, timestamp As Date, time As Decimal, sport As String)
+            Me.resultID = resultID
+            Me.userID = userID
+            Me.timestamp = timestamp
+            Me.time = time
+            Me.sport = sport
+        End Sub
+
+        Public Function getTime() As Decimal
+            Return Me.time
+        End Function
+
+        Public Function getSport() As String
+            Return Me.sport
+        End Function
+
+        Public Function getTimestamp() As Date
+            Console.WriteLine(Me.timestamp.ToString())
+            Return Me.timestamp
+        End Function
+
+        Public Function getUserID() As Integer
+            Return Me.userID
+        End Function
+
+        Public Function getResultID() As Integer
+            Return Me.getResultID
+        End Function
+
+    End Class
+    Private Sub loadResults()
+        Dim resultsLines = My.Computer.FileSystem.ReadAllText(ModResults.resultsLocation).Split(vbLf)
+        Dim lineItems() As String
+        Dim maxUserID As Integer = 0
+
+        Array.Resize(allResults, resultsLines.Length - 1)
+
+        For lineIndex As Integer = 0 To resultsLines.Length - 1
+            lineItems = resultsLines(lineIndex).Split(",")
+            If lineItems.Length = 4 Then
+                allResults(lineIndex) = New result(lineIndex, lineItems(0), DateTime.Parse(lineItems(1)), lineItems(2), lineItems(3))
+            End If
+        Next lineIndex
+        hasLoaded = False
+    End Sub
+
+    Public Function getResults(userID As Integer) As Array
+        If Not hasLoaded Then
+            loadResults()
+        End If
+        Dim retArray() As result = {}
+        For resultID As Integer = 0 To allResults.Length - 1
+            If allResults(resultID).getUserID() = userID Then
+                Array.Resize(retArray, retArray.Length + 1)
+                Console.WriteLine(allResults(resultID))
+                retArray(retArray.Length - 1) = allResults(resultID)
+            End If
+        Next
+        Return retArray
+    End Function
+
+    Public Sub saveResult(userID As Integer, timestamp As DateTime, time As Decimal, sport As String)
+        My.Computer.FileSystem.WriteAllText(resultsLocation, userID & "," & timestamp.ToString() & "," & time & "," & sport & vbLf, True)
+
+    End Sub
+
+    Public Function FileExists() As Boolean
+        Return My.Computer.FileSystem.FileExists(resultsLocation)
+    End Function
+End Module
+
