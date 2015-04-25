66e316ad6e9bacdd21f46e75407728512f08163b
Emati Mitame <gitcommits@mita.me>
2015-04-25 17:36:18 +0100

    Add missing files... VS excluded them

---
+++ Training Logger/FrmUserViewResults.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmUserViewResults
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.LbData = New System.Windows.Forms.ListBox()
+        Me.BtnAdd = New System.Windows.Forms.Button()
+        Me.BtnReload = New System.Windows.Forms.Button()
+        Me.BtnQuit = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 5
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.LbData, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.BtnAdd, 2, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.BtnReload, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.BtnQuit, 3, 1)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(13, 13)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(433, 236)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'LbData
+        '
+        Me.LbData.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.SetColumnSpan(Me.LbData, 5)
+        Me.LbData.FormattingEnabled = True
+        Me.LbData.Items.AddRange(New Object() {"Date" & Global.Microsoft.VisualBasic.ChrW(9) & Global.Microsoft.VisualBasic.ChrW(9) & "Sport" & Global.Microsoft.VisualBasic.ChrW(9) & Global.Microsoft.VisualBasic.ChrW(9) & "Result"})
+        Me.LbData.Location = New System.Drawing.Point(3, 3)
+        Me.LbData.Name = "LbData"
+        Me.LbData.Size = New System.Drawing.Size(427, 199)
+        Me.LbData.TabIndex = 2
+        '
+        'BtnAdd
+        '
+        Me.BtnAdd.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnAdd.AutoSize = True
+        Me.BtnAdd.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BtnAdd.Location = New System.Drawing.Point(189, 210)
+        Me.BtnAdd.Name = "BtnAdd"
+        Me.BtnAdd.Size = New System.Drawing.Size(69, 23)
+        Me.BtnAdd.TabIndex = 1
+        Me.BtnAdd.Text = "Add Result"
+        Me.BtnAdd.UseVisualStyleBackColor = True
+        '
+        'BtnReload
+        '
+        Me.BtnReload.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnReload.AutoSize = True
+        Me.BtnReload.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BtnReload.Location = New System.Drawing.Point(132, 210)
+        Me.BtnReload.Name = "BtnReload"
+        Me.BtnReload.Size = New System.Drawing.Size(51, 23)
+        Me.BtnReload.TabIndex = 4
+        Me.BtnReload.Text = "Reload"
+        Me.BtnReload.UseVisualStyleBackColor = True
+        '
+        'BtnQuit
+        '
+        Me.BtnQuit.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnQuit.AutoSize = True
+        Me.BtnQuit.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BtnQuit.Location = New System.Drawing.Point(264, 210)
+        Me.BtnQuit.Name = "BtnQuit"
+        Me.BtnQuit.Size = New System.Drawing.Size(36, 23)
+        Me.BtnQuit.TabIndex = 3
+        Me.BtnQuit.Text = "Quit"
+        Me.BtnQuit.UseVisualStyleBackColor = True
+        '
+        'FrmUserViewResults
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(458, 261)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "FrmUserViewResults"
+        Me.Text = "Training Logger - View Results"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents BtnAdd As System.Windows.Forms.Button
+    Friend WithEvents LbData As System.Windows.Forms.ListBox
+    Friend WithEvents BtnReload As System.Windows.Forms.Button
+    Friend WithEvents BtnQuit As System.Windows.Forms.Button
+End Class
